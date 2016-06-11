6cfeb40ecf57717dd6041f361722c7c5866fab21
Jim Reed <ire@us-rem-ire.local>
2016-06-11 11:59:47 -0400

    basic web hook that runs mocha test and reports result on slack

--- server.js
+++ server.js
@@ -1,35 +1,39 @@
+//
+// config
+//
+webhookUri = "__replace_url__";
+dir = "__replace_working_dir__";
+
+//
+// run mocha test then report result on slack
+//
 require('shelljs/global');
 var Slack = require('slack-node');
 
-webhookUri = "__replace_url__";
- 
 slack = new Slack();
 slack.setWebhook(webhookUri);
 
-slack.webhook({
-  channel: "#website-status",
-  username: "incoming-webhook",
-  text: "This is posted to #general and comes from a bot named webhookbot."
-}, function(err, response) {
-  console.log(response);
+cd(dir);
+
+exec('./node_modules/.bin/mocha tests | grep " passing\\| failing"', function(code, stdout, stderr) {
+  message = "";
+  if (code == 0) {
+    message = "test results: " + stdout;
+    if (message.indexOf("fail") != -1) {
+      message = "<!channel>: " + message;
+    }
+  } else {
+    message = "<!channel>: exit code:" + code + ", " + stderr;
+  }
+
+  slack.webhook({
+    channel: "#website-status",
+    username: "incoming-webhook",
+    text: message
+  }, function(err, response) {
+    if (err) {
+      console.log(response);
+    }
+  });
+
 });
-
-fs = require('fs');
-
-cd('/opt/chordpro-editor');
-rm('t.out');
-
-if (exec('./node_modules/.bin/mocha tests | grep " passing\\| failing" > t.out').code !== 0) {
-  echo('Error running test');
-} else {
-	fs.readFile('t.out', function(err,data) {
-		if (err) {
-			echo('Error reading t.out');
-		} else {
-			echo(data.toString());
-		}
-	});
-}
-
-
-cd('/opt/slack-if-site-down');
